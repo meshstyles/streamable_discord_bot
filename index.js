0eddfa0e80f8d8444249573ac56831c7dc8995f8
Max M <meshstyles@users.noreply.github.com>
2022-08-31 14:28:39 +0200

    bumping axios and chaning formatting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const axios = require('axios');
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client({ disableEveryone: true });
 
-bot.on("ready", async() => {
+bot.on("ready", async () => {
     console.log(`Bot is online`)
     bot.user.setActivity("type -help for help")
 });
@@ -16,38 +16,40 @@ var urlImport = "https://api.streamable.com/import?url=";
 var urlExport = "https://api.streamable.com/videos/"
 
 bot.on("message", async message => {
-  if(message.content.startsWith(botconfig.convert)){
+    if (message.content.startsWith(botconfig.convert)) {
 
-    var convMsg = message.content.split(" ");
-    
-    urlImport = urlImport + convMsg[1];
-    axios.get(urlImport,{
-      auth: {
-        username: uemail,
-        password: upwd
-      }
-    }).then((resp) => {
-      message.channel.send( streamableBaselinkVideo + resp.data.shortcode);
-    },(error) => {
-      console.log(error);
-    });
-  }
-  
-  if(message.content.startsWith(botconfig.retrieve)){
-    var retMsg = message.content.split(streamableBaselinkVideo)
-    urlExport = urlExport + retMsg[1];
+        var convMsg = message.content.split(" ");
 
-    axios.get(urlExport,{
-      auth: {
-        username: uemail,
-        password: upwd
-      }
-    }).then((resp) => {
-      message.channel.send("https:"+resp.data.files.mp4.url);
-    },(error) => {
-      console.log(error);
-    });
-  }
+        urlImport = urlImport + convMsg[1];
+        axios.get(urlImport, {
+            auth: {
+                username: uemail,
+                password: upwd
+            }
+        }).then((resp) => {
+            message.channel.send(streamableBaselinkVideo + resp.data.shortcode);
+        }, (error) => {
+            console.log(error);
+        });
+    }
+
+    if (message.content.startsWith(botconfig.retrieve)) {
+        var retMsg = message.content.split(streamableBaselinkVideo)
+        console.log(retMsg[1]);
+        urlExport = urlExport + retMsg[1];
+
+        axios.get(urlExport, {
+            auth: {
+                username: uemail,
+                password: upwd
+            }
+        }).then((resp) => {
+            message.channel.send("https:" + resp.data.files.mp4.url);
+        }, (error) => {
+            console.error(error);
+            // console.log(error);
+        });
+    }
 
 });
 
